64f032a63270e7c23e18e3609fd89a47f1751d59
rreennaann345 <00001119396852sp@al.educacao.sp.gov.br>
2024-11-16 07:59:09 -0300

    teste para um jogo de perguntas

---
+++ segnda pasta/assets/img/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é um linguagem de computador' 
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da polonia?',
+    'A capital da polonia é varsovia'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função',
+    'Uma função é um bloco de codigo que executa alguma função?'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem desenvolveu a teoria da evolução?',
+    'Charles Darwin',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a porcentagem de agua no corpo humano?',
+    'Cerca de 70% de agua',
+)
+
+criaCartao(
+    'Matematica',
+    'Quais são as operações basicas?',
+    'Adição, subtração, divisão e multiplicação',
+)
+
+criaCartao(
+    'Quimica',
+    'O que é esquiometria?',
+    'A esquiometria estuda a quantidade de materia em atomos',
+)
+
+criaCartao(
+    'Historia',
+    'Quem era conhecido como a Raposa do Deserto?',
+    'Erwin Rommel',
+)
+
+criaCartao(
+    'Etimologia',
+    'Qual a etmologia da palavra musculo?',
+    'Vem do grego musculus que significa ratinho,
+)
+
